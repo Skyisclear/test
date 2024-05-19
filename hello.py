41930c576f846fa6ae4c72a5777d8cac01a72815
Skyisclear <johnkwon0507@gmail.com>
2024-05-19 19:26:53 +0900

    reset

--- hello.py
+++
@@ -1,6 +0,0 @@
-print("hello world")
-print("apple")
-for i in range(5):
-    print("*" * i)
-
-print("orange")
--- hello_real.py
+++
@@ -1,8 +0,0 @@
-print("hello")
-print("really")
-
-for _ in range(3):
-    print("happy")
-print("joy!")
-
-c = d = 4
